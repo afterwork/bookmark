080ced4bbff7ffc6512461b851c35b0ddb5f5409
vmiron <mironvitaliev@gmail.com>
2019-03-25 21:11:05 +0200

    verify user data and authenticate user

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,30 +1,63 @@
-from django.contrib.auth import get_user_model
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets, status, permissions, generics, mixins
-from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
 from django.conf import settings
+from django.contrib.auth import get_user_model, login
 from django.views.generic import TemplateView
-from apps.common.views import CustomViewSetMixin, CustomSerializerViewSetMixin
-from . import serializers, models
+from django_telegram_login.authentication import verify_telegram_authentication
+from django_telegram_login.errors import NotTelegramDataError, TelegramDataIsOutdatedError
+from django_telegram_login.widgets.constants import LARGE
+from django_telegram_login.widgets.generator import create_redirect_login_widget
+from rest_framework import viewsets
 
-from django_telegram_login.widgets.constants import SMALL, MEDIUM, LARGE, DISABLE_USER_PHOTO
-from django_telegram_login.widgets.generator import create_callback_login_widget, create_redirect_login_widget
+from apps.common.views import CustomViewSetMixin
+from . import serializers
 
 
 class TelegramLoginView(TemplateView):
-
     template_name = "users/login.html"
 
     def get_context_data(self, **kwargs):
-        bot_name = settings.TELEGRAM_BOT_NAME
-        bot_token = settings.TELEGRAM_BOT_TOKEN
-        redirect_url = settings.TELEGRAM_LOGIN_REDIRECT_URL
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in the publisher
-        context["telegram_login_widget"] = create_callback_login_widget(bot_name, size=SMALL)
+        context["telegram_login_widget"] = create_redirect_login_widget(
+            settings.TELEGRAM_LOGIN_REDIRECT_URL, settings.TELEGRAM_BOT_NAME, size=LARGE
+        )
+        return context
+
+
+class RegisterUser(TemplateView):
+    template_name = "users/login_data.html"
+
+    def check_data(self, **kwargs):
+        context = super().get_context_data()
+        if not self.request.GET.get("hash"):
+            context["error"] = True
+            context["message"] = "Handle the missing Telegram data in the response."
+            return context
+        try:
+            result = verify_telegram_authentication(
+                bot_token=settings.TELEGRAM_BOT_TOKEN, request_data=self.request.GET
+            )
+        except TelegramDataIsOutdatedError:
+            context["error"] = True
+            context["message"] = "Authentication was received more than a day ago."
+            return context
+
+        except NotTelegramDataError:
+            context["error"] = True
+            context["message"] = "The data is not related to Telegram!"
+            return context
+
+        user_model = get_user_model()
+
+        try:
+            user = user_model.objects.get(username=result["id"])
+        except user_model.DoesNotExist:
+            user = user_model.objects.create_user(
+                username=result.get("id"), first_name=result.get("first_name"), last_name=result.get("last_name")
+            )
+        login(request=self.request, user=user)
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = self.check_data(**kwargs)
         return context
 
 
