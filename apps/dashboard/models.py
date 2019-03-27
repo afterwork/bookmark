d3da6c32975778055c3dce5bb46dd9eb9608a639
vmiron <mironvitaliev@gmail.com>
2019-03-27 18:08:00 +0200

    add bookmarks model

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from jsonfield import JSONField
+from django.views.generic import TemplateView
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100)
+    thumbnail = models.CharField(max_length=100)
+    content = models.TextField()
+    url = models.CharField(max_length=100)
+    tags = JSONField()
+
+    def __str__(self):
+        return self.title
