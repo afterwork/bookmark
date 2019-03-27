65d846a7fd143ec4f5b267bc8aca250c8b0f099d
vmiron <mironvitaliev@gmail.com>
2019-03-27 12:29:22 +0200

    add logout url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,7 +8,7 @@ router = SimpleRouter(trailing_slash=False)
 router.register("users", viewset=views.UserViewSet)
 
 urlpatterns = [
-    path("login", TokenObtainSlidingView.as_view(), name="token_obtain"),
+    path("logout", views.Logout.as_view(), name="logout"),
     path("token/refresh", TokenRefreshSlidingView.as_view(), name="token_refresh"),
     path("login/telegram", views.TelegramLoginView.as_view(), name="telegram_login"),
     path("login/telegram/redirect", views.RegisterUser.as_view(), name="telegram_login_redirect"),
