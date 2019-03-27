20ca8ad99ac4e93c51d4977d2c7a77102c6de3da
vmiron <mironvitaliev@gmail.com>
2019-03-27 15:16:36 +0200

    remove routers

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import include
 from django.urls import path
-from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import TokenObtainSlidingView, TokenRefreshSlidingView, TokenVerifyView
 from . import views
 
 
