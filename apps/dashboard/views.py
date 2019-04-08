0d04b0956a355ecd04d942e15b6855eadc483cef
vmiron <mironvitaliev@gmail.com>
2019-04-08 17:56:40 +0300

    use login required mixin

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.models import User
 from django.views import View
-from django.http import HttpResponse
-from django.shortcuts import redirect
-from django.urls import reverse
+from django.urls import reverse_lazy
 from .models import Bookmark
+from .forms import BookmarkForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
-class DashHome(View):
+class DashHome(LoginRequiredMixin, View):
     template_name = "dashboard/home.html"
     context = dict()
+    login_url = reverse_lazy("telegram_login")
 
     def get(self, request):
         user = request.user
-        if not user.is_authenticated:
-            return redirect(reverse("telegram_login"))
-        else:
-            book = Bookmark.objects.all()
-            self.context["book"] = book
-            self.context["first_name"] = user.first_name
-            self.context["last_name"] = user.last_name
-            self.context["photo_url"] = user.photo_url
+        book = Bookmark.objects.all().filter(author=request.user)
+        self.context["book"] = book
+        self.context["first_name"] = user.first_name
+        self.context["last_name"] = user.last_name
+        self.context["photo_url"] = user.photo_url
 
         return render(request, "dashboard/home.html", self.context)
