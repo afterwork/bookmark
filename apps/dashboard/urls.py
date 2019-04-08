daaad28b87d21772a0d6d1cb9369126035464f1b
vmiron <mironvitaliev@gmail.com>
2019-04-08 18:55:11 +0300

    add create bookmark page

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.DashHome.as_view(), name="home")]
+urlpatterns = [
+    path("", views.DashHome.as_view(), name="home"),
+    path("create/", views.BookmarkFormView.as_view(), name="create_bookmark"),
+]
