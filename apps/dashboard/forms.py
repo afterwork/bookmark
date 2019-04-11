7ab5324d83a395390a65249a907598cefda372b8
vmiron <mironvitaliev@gmail.com>
2019-04-11 10:55:43 +0300

    add validation for fields

--- apps/dashboard/forms.py
+++ apps/dashboard/forms.py
@@ -1,8 +1,22 @@
 from .models import Bookmark
 from django import forms
+from django.core.validators import URLValidator, RegexValidator
+from django.core.exceptions import ValidationError
 
 
 class BookmarkForm(forms.ModelForm):
     class Meta:
         model = Bookmark
         fields = ["title", "thumbnail", "content", "url"]
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title", False)
+        if len(title.split()) < 2:
+            raise forms.ValidationError("Title should contain at least 2 words")
+        return title
+
+    def clean_url(self):
+        url = self.cleaned_data.get("url", False)
+        validate_url = URLValidator()
+        validate_url(url)
+        return url
